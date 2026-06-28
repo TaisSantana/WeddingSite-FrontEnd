e16759adacc7650a4a90680e3e71016a7ef326ea
TaisSantana <tais_santana002@hotmail.com>
2026-06-28 13:03:45 -0300

    mudando link back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://weddingsite-backend-production.up.railway.app/api',
+    //apiUrl: 'https://weddingsite-backend-production.up.railway.app/api',
+    apiUrl: 'https://weddingsite-backend.onrender.com/api',
     weddingDate: '2026-09-19T15:00:00',
     pixKey: 'tatahgabe.wedding@gmail.com',
     pixName: 'Taís e Gabriel',
